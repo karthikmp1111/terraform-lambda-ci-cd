8622d524dd169443421df088e32e62dc7bce6ec3
karthikeyan <karthikeyan@bicsglobal.com>
2025-03-19 16:46:30 +0530

    modified lanbda function

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -12,14 +12,14 @@ data "aws_secretsmanager_secret_version" "current_redshift_secret_version" {
 resource "aws_secretsmanager_secret_version" "redshift_secret_version" {
   secret_id     = data.aws_secretsmanager_secret.redshift_secret.id  # Reference the existing secret by ID
   secret_string = jsonencode({
-    REDSHIFT_USER     = jsondecode(data.aws_secretsmanager_secret_version.current_redshift_secret_version.secret_string).REDSHIFT_USER,  # Keep the current user
-    REDSHIFT_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.current_redshift_secret_version.secret_string).REDSHIFT_PASSWORD,  # Keep the current password
+    REDSHIFT_USER     = jsondecode(data.aws_secretsmanager_secret_version.current_redshift_secret_version.secret_string).REDSHIFT_USER,
+    REDSHIFT_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.current_redshift_secret_version.secret_string).REDSHIFT_PASSWORD,
     REDSHIFT_HOST     = split(":", aws_redshift_cluster.security_cluster.endpoint)[0],
-    REDSHIFT_DBNAME   = jsondecode(data.aws_secretsmanager_secret_version.current_redshift_secret_version.secret_string).REDSHIFT_DBNAME,  # Keep the current DB name
-    REDSHIFT_PORT     = jsondecode(data.aws_secretsmanager_secret_version.current_redshift_secret_version.secret_string).REDSHIFT_PORT  # Keep the current port (5439)
+    REDSHIFT_DBNAME   = jsondecode(data.aws_secretsmanager_secret_version.current_redshift_secret_version.secret_string).REDSHIFT_DBNAME,
+    REDSHIFT_PORT     = jsondecode(data.aws_secretsmanager_secret_version.current_redshift_secret_version.secret_string).REDSHIFT_PORT
   })
 
   depends_on = [
-    aws_redshift_cluster.security_cluster  # Ensure Redshift cluster is created before updating the secret
+    aws_redshift_cluster.security_cluster
   ]
 }
